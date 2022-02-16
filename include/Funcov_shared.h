af129db9a7eaaf52a778a6a4abb8c69a9155851b
Hwangwoosam <sam1783@naver.com>
2022-02-16 16:27:51 +0900

    modify structure

--- include/Funcov_shared.h
+++ include/Funcov_shared.h
@@ -4,17 +4,28 @@
 #define MAX_ADDR 256
 #define MAX_BUF 4096
 #define MAX_NUM 1000
-#define HASH_SIZE 6000
-
-typedef struct function_hit* func_link;
+#define HASH_SIZE 65536
 
 typedef struct function_hit{
-    char func_line[512]; //calle + caller + line number;
-    
-    int pt_line;        
 
-    int hit;
+    char func_line[512]; //calle + caller + caller address
+    int hit_cnt;
+
 }function_hit_t;
+
+typedef struct union_coverage{
+
+    char* total_coverage[HASH_SIZE]; 
+    char* line_number[HASH_SIZE];
+
+}union_coverage_t;
+
+typedef struct indiv_coverage{
+    int* func_list;
+    int* hit_cnt;
+    int func_cnt;
+    int union_cnt;
+}indiv_coverage_t;
 
 typedef struct prog_info{
     char out_dir[MAX_ADDR];
--- include/shared_memory.h
+++ include/shared_memory.h
@@ -6,11 +6,12 @@
 #include "Funcov_shared.h"
 
 typedef struct SHM_info{
-    function_hit_t func_union[HASH_SIZE][20];
-    int func_num[HASH_SIZE];
+
+    function_hit_t func_coverage[HASH_SIZE];
+    int cnt;
+
 }SHM_info_t;
 
-unsigned hash(char* name);
 int shm_alloc(int opt);
 void shm_dettach(SHM_info_t* shm_info);
 void shm_dealloc(int shmid);
