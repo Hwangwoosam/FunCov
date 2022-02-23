827ecf20b48aa13e5fd328a44ce14665e55c34b1
Hwangwoosam <sam1783@naver.com>
2022-02-23 13:26:01 +0900

    add input option(file-argument) and error case

--- include/Funcov_shared.h
+++ include/Funcov_shared.h
@@ -1,23 +1,22 @@
 #ifndef _FUNCOV_H_
 #define _FUNCOV_H_
 
-#define MAX_ADDR 256
-#define MAX_BUF 4096
+#define MAX_ADDR 255
+#define MAX_BUF 1024
 #define MAX_NUM 1000
 #define HASH_SIZE 65536
 
+#define STDIN 0x0
+#define ARG_FILE 0x1
+
 typedef struct function_hit{
-
-    char func_line[512]; //calle + caller + caller address
-    int hit_cnt;
-
+    char func_line[MAX_BUF]; //calle + caller + caller address
+    int hit_cnt;             //function hit count
 }function_hit_t;
 
 typedef struct union_coverage{
-
     char* total_coverage[HASH_SIZE]; 
     char* line_number[HASH_SIZE];
-
 }union_coverage_t;
 
 typedef struct indiv_coverage{
@@ -35,6 +34,7 @@ typedef struct prog_info{
 
     int inputs_num;
     int trial;
+    int input_type;
 
 }prog_info_t;
 
